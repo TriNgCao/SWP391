4f42b926c0ad7369cdc41145c0974de0e0987992
TriNgCao <tringuyencao.se@gmail.com>
2024-10-02 21:26:26 +0700

    source code BE HairSalon Booking

---
+++ SWP391_BE/src/test/java/com/swp391/hairsalon/Swp391ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.hairsalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
