0b0e0a539f8741abfec51e64c26ee8627af36270
Alex Bien <hoidev.codes@gmail.com>
2024-10-09 20:00:41 +0700

    update

--- SWP391_FE_React/myapp/src/App.js
+++ SWP391_FE_React/myapp/src/App.js
@@ -1,14 +1,23 @@
-import Footer from "./componets/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "./componets/Home";
-import Navbar from "./componets/NavBar";
+
+import AboutUs from "./componets/AboutUs";
+import Contact from "./componets/Contact";
+import Stylist from "./componets/Stylist";
 
 function App() {
   return (
     <div>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </div>     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/stylist" element={<Stylist />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
