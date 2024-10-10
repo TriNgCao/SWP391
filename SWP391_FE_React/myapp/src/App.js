8c0c20c40b57f7ad7efece1d8dea69c62d9c9365
Alex Bien <hoidev.codes@gmail.com>
2024-10-11 00:25:01 +0700

    Update dashboard

--- SWP391_FE_React/myapp/src/App.js
+++ SWP391_FE_React/myapp/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./componets/Home";
+import Home from "./components/Home";
 
-import AboutUs from "./componets/AboutUs";
-import Contact from "./componets/Contact";
-import Stylist from "./componets/Stylist";
+import AboutUs from "./components/AboutUs";
+import Contact from "./components/Contact";
+import Stylist from "./components/Stylist";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/stylist" element={<Stylist />} />
+          {/* TEST DASHBOARD KHÔNG XÓA NHA !!!! */}
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
