69f9df2d9cf4cd029bf38669ab337b4c1576ba14
Alex Bien <hoidev.codes@gmail.com>
2024-10-15 00:12:44 +0700

    Finished 95% Manager Dashboard

--- SWP391_FE_React/myapp/src/App.js
+++ SWP391_FE_React/myapp/src/App.js
@@ -1,61 +1,47 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import AboutUs from "./components/AboutUs";
-import Contact from "./components/Contact";
-import Stylist from "./components/Stylist";
-import Payroll from "./components/Dashboard/Manage/Payroll";
-import Transaction from "./components/Dashboard/Manage/Transaction";
-import AdminDashboard from "./components/Dashboard/Admin/AdminDashboard";
-import Manage_Appointments from "./components/Dashboard/Staff/Manage_Appointments";
-import Profile from "./components/Dashboard/Profile";
-import Manage_Account from "./components/Dashboard/Manage_Account";
-import ManageDashboard from "./components/Dashboard/Manage/ManageDashboard";
-import Revenue from "./components/Dashboard/Manage/Revenue";
-import StaffDashboard from "./components/Dashboard/Staff/StaffDashboard";
-import Salary from "./components/Dashboard/Salary";
-import Schedule from "./components/Dashboard/Stylist/Schedule";
-import StylistDashboard from "./components/Dashboard/Stylist/StylistDashboard";
-import Services from "./components/Dashboard/Manage/Services";
+import Home from "./components/Customer/Home";
+import AboutUs from "./components/Customer/AboutUs";
+import Contact from "./components/Customer/Contact";
+import Stylist from "./components/Customer/Stylist";
+import ManagerAppointments from "./Pages/Manager/ManagerAppointments";
+import ManagerPayroll from "./Pages/Manager/ManagerPayroll";
+import ManagerLayout from "./Layouts/Manager/ManagerLayout";
+import ManagerProfilePage from "./Pages/Manager/ManagerProfilePage";
+import ManageRevenuePage from "./Pages/Manager/ManagerRevenuePage";
+import ManagerTransaction from "./Pages/Manager/ManagerTransaction";
+import Header from "./components/Dashboard/Header";
+
+import BreadcrumbsHeader from "./components/Dashboard/breadcum";
+import ManagerServices from "./Pages/Manager/ManagerServices";
+import ManagerPersonnel from "./Pages/Manager/ManagerPersonnel";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
+        <BreadcrumbsHeader />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/stylist" element={<Stylist />} />
-          {/* ADMIN  */}
-          <Route path="/admin" element={<AdminDashboard />}>
-            <Route path="profile" element={<Profile />}></Route>
-            <Route path="manage-account" element={<Manage_Account />} />
-          </Route>
-          {/* MANAGE */}
-          <Route path="/manager" element={<ManageDashboard />}>
-            <Route path="profile" element={<Profile />}></Route>
-            <Route path="revenue" element={<Revenue />}></Route>
-            <Route path="services" element={<Services />}></Route>
-            <Route path="manage-account" element={<Manage_Account />} />
-            <Route path="transaction" element={<Transaction />} />
-            <Route path="payroll" element={<Payroll />} />
-          </Route>
-          {/* STAFF */}
-          <Route path="/staff" element={<StaffDashboard />}>
-            <Route path="profile" element={<Profile />}></Route>
-            <Route
-              path="manage-appointments"
-              element={<Manage_Appointments />}
-            />
 
-            <Route path="transaction" element={<Transaction />} />
-            <Route path="salary" element={<Salary />} />
-          </Route>
-          {/* STYLIST */}
-          <Route path="/stylistdashboard" element={<StylistDashboard />}>
-            <Route path="profile" element={<Profile />} />
-            <Route path="salary" element={<Salary />} />
-            <Route path="schedule" element={<Schedule />} />
+          {/* DASHBOARD MANAGER */}
+          <Route path="/manager" element={<ManagerLayout />}>
+            <Route path="manager-services" element={<ManagerServices />} />
+            <Route path="manager-profile" element={<ManagerProfilePage />} />
+            <Route path="manager-personnel" element={<ManagerPersonnel />} />
+            <Route path="manager-revenue" element={<ManageRevenuePage />} />
+            <Route path="manager-payroll" element={<ManagerPayroll />} />
+            <Route
+              path="manager-transaction"
+              element={<ManagerTransaction />}
+            />
+            <Route
+              path="manager-appointments"
+              element={<ManagerAppointments />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
