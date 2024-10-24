a4bf85fd4651dd4c4f288440103dc9e9b1ff6048
hoangngoctien04 <hoangngoctien04@gmail.com>
2024-10-24 13:20:00 +0700

    Update Revenue

--- SWP391_BE/src/main/java/com/swp391/hairsalon/controller/RevenueController.java
+++ SWP391_BE/src/main/java/com/swp391/hairsalon/controller/RevenueController.java
@@ -21,14 +21,12 @@ public class RevenueController {
     @Autowired
     private IRevenueService revenueService;
 
-    // API để lấy doanh thu tổng hợp từ tất cả các lịch hẹn
     @PostMapping("/total")
     public ResponseEntity<Revenue> getTotalRevenue(@RequestBody List<Appointment> appointments) {
         Revenue revenue = revenueService.getRevenueData(appointments);
         return ResponseEntity.ok(revenue);
     }
 
-    // API để tính doanh thu giữa hai ngày
     @GetMapping("/between/{startDate}/{endDate}")
     public ResponseEntity<BigDecimal> getRevenueBetweenDates(@PathVariable String startDate, @PathVariable String endDate) {
         try {
@@ -43,28 +41,24 @@ public class RevenueController {
         }
     }
 
-    // API để tính doanh thu trong X ngày gần đây
     @GetMapping("/recent/{days}")
     public ResponseEntity<BigDecimal> getRevenueForRecentDays(@PathVariable int days) {
         BigDecimal revenue = revenueService.calculateRevenueForRecentDays(days);
         return ResponseEntity.ok(revenue);
     }
 
-    // API để tính doanh thu hàng ngày
     @PostMapping("/daily")
     public ResponseEntity<List<DailyRevenue>> getDailyRevenue(@RequestBody List<Appointment> appointments) {
         List<DailyRevenue> dailyRevenues = revenueService.calculateDailyRevenues(appointments);
         return ResponseEntity.ok(dailyRevenues);
     }
 
-    // API để tính tổng doanh thu từ các lịch hẹn
     @PostMapping("/total-amount")
     public ResponseEntity<BigDecimal> calculateTotalRevenue(@RequestBody List<Appointment> appointments) {
         BigDecimal totalRevenue = revenueService.calculateTotalRevenue(appointments);
         return ResponseEntity.ok(totalRevenue);
     }
 
-    // API để tính tổng lợi nhuận từ các lịch hẹn
     @PostMapping("/total-profit")
     public ResponseEntity<BigDecimal> calculateTotalProfit(@RequestBody List<Appointment> appointments) {
         BigDecimal totalProfit = revenueService.calculateTotalProfit(appointments);
